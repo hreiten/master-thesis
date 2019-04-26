961368fbdcea33c07daad0c85adb3b20ec577592
= <thomas@isvik.no>
2019-04-26 15:21:37 +0200

    fixup: merge benchmark models and anomaly model into develop
    Closes: #31
    
    Signed-off-by: = <thomas@isvik.no>

---
+++ src/anomaly-detection/settings.py
@@ -0,0 +1,10 @@
+UPPER_ANOMALY_THRESHOLD = 0.90
+LOWER_ANOMALY_THRESHOLD = -0.1
+NUM_AVERAGED_ANOMALY_SCORES = 5
+WINDOW_LENGTH = 50
+INPUT_FILE_PATH = './data/bsts_prediction.csv'
+MIN_NUMBER_OF_RESIDUALS = 1000
+DISTRIBUTIONS = ["norm", "weibull_max", "weibull_min", "pareto", "genextreme", 'gamma', 'beta', 'rayleigh']
+DEFAULT_BEST_DIST = "norm"
+ALPHA = 1E-3
+
