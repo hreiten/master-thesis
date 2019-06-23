7a27462b73bfef5100ce512c9ea4013f38861a91
Halvor Reiten <ha.reiten@gmail.com>
2019-06-24 01:31:12 +0200

    Added scaling stats and selected tags as separate files

--- shell-scripts/bash/open_jupyter.sh
+++
@@ -1,3 +0,0 @@
-port=$(($port + $2))
-printf "Opening port: $port \n"
-screen -r $1 -X stuff $'jupyter notebook --no-browser --port='$port' --NotebookApp.token='' --NotebookApp.password=''\n'
--- shell-scripts/bash/params.sh
+++
@@ -1,8 +0,0 @@
-base_ip_u2=129.241.184
-base_ip_banach=129.241.211
-markov=markov.math.ntnu.no
-user=thomklei
-path=/home/shomeb/t/thomklei/master_thesis
-default_port=8080
-default_computer=60
-default_screen_name="default"
